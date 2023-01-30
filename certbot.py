b4aa78dccc85ba3209ae1a99257589cc50956399
Giuseppe Pagano <giuseppe.pagano.p@gmail.com>
2023-01-30 16:29:48 +0100

    add some command-line ui

--- certbot.py
+++ certbot.py
@@ -1,13 +1,25 @@
 from dns_client import DNSClient
 from config import StoreManager
 import time
+import re
 
 challenge_hostname = "_acme-challenge"
+
+def is_domain_or_subdomain(sld: str, to_test: str):
+    domain = sld.replace(".", "\.")
+    regexp = "([\d\w]\.)*{}$".format(domain)
+    if re.search(regexp, to_test) is not None:
+        to_test = to_test.replace(sld, "")
+        if to_test == "":
+            return True
+        return to_test[-1] == "."
+    return False
 
 def dns_challenge(client: DNSClient, conf: StoreManager):
     certbot_domain = conf.get("CERTBOT_DOMAIN")
     personal_domain = conf.get("DNS_DOMAIN")
-    if certbot_domain != personal_domain:
+    # personal_domain is a sld while certbot_domain can be a subdomain
+    if is_domain_or_subdomain(personal_domain, certbot_domain):
         print("uknown requested domain")
         exit(-1)
     challenge_response = conf.get("CERTBOT_VALIDATION")
--- main.py
+++ main.py
@@ -2,10 +2,43 @@
 
 from dns_client import DNSClient
 from synchronizers import DnsASynchronizer
+from certbot import dns_challenge, dns_cleanup
 from config import EnvStore, StoreManager
+from enum import Enum
 
-config = StoreManager()
-config.add_store(EnvStore())
+import argparse
+
+def init_config():
+    config = StoreManager()
+    config.add_store(EnvStore())
+    return config
+
+class Commands(Enum):
+    SYNC = "sync"
+    CERTBOT  = "certbot"
+
+    def __str__(self):
+        return self.value
+
+class CertbotActions(Enum):
+    CHALLENGE = "challenge"
+    CLEANUP  = "cleanup"
+
+    def __str__(self):
+        return self.value
+
+
+def build_parser():
+    parser = argparse.ArgumentParser(prog='swissdns')
+    sub_parsers = parser.add_subparsers(dest="command", required=True, help='sub-command help')
+    sync = sub_parsers.add_parser(Commands.SYNC.value, help='make one or more dns record(s) point to your public ip')
+
+    certbot = sub_parsers.add_parser(Commands.CERTBOT.value, help='handle certbot dns challenge')
+    certbot.add_argument("mode", type=CertbotActions, choices=list(CertbotActions))
+
+    return parser.parse_args()
+
+config = init_config()
 
 username = config.get("DNS_REGISTRAR_USERNAME")
 token = config.get("DNS_REGISTRAR_TOKEN")
@@ -13,7 +46,15 @@ domain = config.get("DNS_DOMAIN")
 provider = config.get("DNS_PROVIDER")
 hosts = config.get("DNS_HOST_TO_REFRESH").split(" ")
 
+args = build_parser()
 dns_client = DNSClient(provider, username, token)
-syncher = DnsASynchronizer(dns_client, domain, hosts)
 
-syncher.sync()
+if args.command == Commands.SYNC.value:
+    syncher = DnsASynchronizer(dns_client, domain, hosts)
+    syncher.sync()
+elif args.command == Commands.CERTBOT.value:
+    if args.mode == CertbotActions.CHALLENGE:
+        dns_challenge(dns_client, config)
+    elif args.mode == CertbotActions.CLEANUP:
+        dns_cleanup(dns_client)
+
